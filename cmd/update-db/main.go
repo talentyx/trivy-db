3b1e8692134e2d1b5b26c46d7c61db61d85da3b6
yangxin15 <yangxin15@lenovo.com>
2023-01-12 15:45:47 +0800

    添加日志

--- cmd/update-db/main.go
+++ cmd/update-db/main.go
@@ -18,9 +18,13 @@ const CHN_DB string = "cn_vuln.db"
 func main() {
 
 	trivydb := vdb.NewVulnDB()
-	trivydb.OpenDatabase(TRIVY_DB)
+	if err := trivydb.OpenDatabase(TRIVY_DB); err != nil {
+		fmt.Println(err.Error())
+	}
 	chndb := vdb.NewVulnDB()
-	chndb.OpenDatabase(CHN_DB)
+	if err := chndb.OpenDatabase(CHN_DB); err != nil {
+		fmt.Println(err.Error())
+	}
 	defer func() {
 		fmt.Println("close connect")
 		if trivydb != nil {
@@ -30,6 +34,8 @@ func main() {
 			chndb.CloseDatabase()
 		}
 	}()
+	noMatch := 0
+	totalChange := 0
 	chndb.DB.View(func(tx *bbolt.Tx) error {
 		buckets := tx.Bucket([]byte(vdb.BUCKET_NAME))
 		buckets.ForEach(func(k, v []byte) error {
@@ -37,6 +43,7 @@ func main() {
 			json.Unmarshal(v, cve)
 			m := trivydb.GetVulnByCveId("vulnerability", string(k))
 			changed := false
+
 			if len(m) > 0 {
 				if title, ok := m["Title"].(string); ok {
 					if len(cve.Title) > 0 && len(title) > 0 {
@@ -53,14 +60,18 @@ func main() {
 				if changed {
 					if mb, err := json.Marshal(&m); err == nil {
 						fmt.Println(fmt.Sprintf("update key %s", string(k)))
+						totalChange++
 						trivydb.SaveVuln("vulnerability", string(k), mb)
 					}
+				} else {
+					noMatch++
 				}
 
 			}
 			return nil
 		})
-
+		fmt.Printf("update total cve count: %d", totalChange)
+		fmt.Printf("Not match cve count: %d", noMatch)
 		return nil
 	})
 
